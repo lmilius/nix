306bf8302fa8fb4fbbc024a4be14091815f2961d
Luke Milius <lmilius@users.noreply.github.com>
2024-04-21 21:08:02 -0500

    Update init_new_system.sh

--- init_new_system.sh
+++ init_new_system.sh
@@ -1,9 +1,11 @@
 # Assuming your system is x86_64-linux
 hostname=nix-cache
 
-sudo nix \
-    --extra-experimental-features 'flakes nix-command' \
-    run github:nix-community/disko#disko-install -- \
-    --flake "github:lmilius/nix?dir=flakes#$hostname" \
-    --write-efi-boot-entries \
-    --disk main /dev/sda
+# sudo nix \
+#     --extra-experimental-features 'flakes nix-command' \
+#     run github:nix-community/disko#disko-install -- \
+#     --flake "github:lmilius/nix?dir=flakes#$hostname" \
+#     --write-efi-boot-entries \
+#     --disk main /dev/sda
+
+sudo nixos-rebuild boot --flake "github:lmilius/nix?dir=flakes#$hostname"
