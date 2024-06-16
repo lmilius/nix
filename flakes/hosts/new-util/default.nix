acb4050b3481f9e1d6cf4aefb50824cf67de85b8
Luke Milius <lmilius12@gmail.com>
2024-06-16 16:59:29 -0500

    new-util use localhost cache

--- flakes/hosts/new-util/default.nix
+++ flakes/hosts/new-util/default.nix
@@ -20,6 +20,11 @@
       })
     ];
 
+  # Use local nix cache
+  nix.settings.substituters = [ 
+    "http://127.0.0.1"
+  ];
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.graceful = true;
