b5e6ca60d2e0021c8a588b4b522dcd1bfdc17480
Luke Milius <lmilius12@gmail.com>
2024-07-27 09:21:30 -0500

    revert back

--- flakes/modules/restic/default.nix
+++ flakes/modules/restic/default.nix
@@ -5,16 +5,16 @@ let
 in
 {
   age.secrets = {
-    "${hostname}/restic/repo".file = "../../secrets/${hostname}/restic/repo.age";
-    "${hostname}/restic/password".file = "../../secrets/${hostname}/restic/password.age";
+    "restic/repo".file = "../../secrets/restic/repo.age";
+    "restic/password".file = "../../secrets/restic/password.age";
   };
 
   services.restic.backups = {
     daily = {
       initialize = true;
 
-      repositoryFile = config.age.secrets."${hostname}/restic/repo".path;
-      passwordFile = config.age.secrets."${hostname}/restic/password".path;
+      repositoryFile = config.age.secrets."restic/repo".path;
+      passwordFile = config.age.secrets."restic/password".path;
 
       paths = [
         "${home_dir}"
@@ -22,6 +22,7 @@ in
 
       exclude = [
         "${home_dir}/.local/share/Steam"
+        "${home_dir}/workspace"
       ];
 
       pruneOpts = [
--- flakes/secrets/secrets.nix
+++ flakes/secrets/secrets.nix
@@ -2,6 +2,6 @@ let
   ssh_key = (builtins.readFile ~/.ssh/id_ed25519.pub);
 in
 {
-  "t480s/restic/password.age".publicKeys = [ ssh_key ];
-  "t480s/restic/repo.age".publicKeys = [ ssh_key ];
+  "restic/password.age".publicKeys = [ ssh_key ];
+  "restic/repo.age".publicKeys = [ ssh_key ];
 }
