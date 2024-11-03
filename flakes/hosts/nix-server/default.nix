7f227a4bdb01049e4d94629ad7e3970aef3683c8
Luke Milius <lmilius12@gmail.com>
2024-11-03 12:37:18 -0600

    update hostId

--- flakes/hosts/nix-server/default.nix
+++ flakes/hosts/nix-server/default.nix
@@ -17,7 +17,7 @@
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.forceImportRoot = false;
   boot.zfs.extraPools = [ "tank" ];
-  networking.hostId = "d131645e";
+  networking.hostId = "aeb247be";
   services.zfs.autoScrub.enable = true;
 
   networking.hostName = hostname; # Define your hostname.
