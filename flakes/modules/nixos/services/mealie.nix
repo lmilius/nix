c70bce0cfcccc6cda587305b874747280d598341
Luke Milius <lmilius12@gmail.com>
2024-11-11 22:41:44 -0600

    fix port

--- flakes/modules/nixos/services/mealie.nix
+++ flakes/modules/nixos/services/mealie.nix
@@ -4,7 +4,7 @@
     enable = true;
     package = pkgs.unstable.mealie;
     listenAddress = "127.0.0.1";
-    port = "9000";
+    port = 9000;
     settings = {
       ALLOW_SIGNUP = "false";
     };
