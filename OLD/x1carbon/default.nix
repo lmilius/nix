5ca015ed55630efe32eb5680fdaea2226a0e1b54
Luke Milius <lmilius12@gmail.com>
2024-12-17 09:53:22 -0600

    Remove sound to comply with 24.11

--- OLD/x1carbon/default.nix
+++ OLD/x1carbon/default.nix
@@ -87,7 +87,7 @@
   };
 
   # Enable sound with pipewire.
-  sound.enable = true;
+  # sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
