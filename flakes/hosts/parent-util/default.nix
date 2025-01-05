d7683b2c22523c4d8a2c7d1a3d6918957a93042b
Luke Milius <lmilius12@gmail.com>
2025-01-05 08:29:49 -0600

    Add android tools to parent-util

--- flakes/hosts/parent-util/default.nix
+++ flakes/hosts/parent-util/default.nix
@@ -107,6 +107,12 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    android-tools
+  ];
+
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
