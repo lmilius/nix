8bdfb769ed93fcba0af8a10d9df49ffe08320992
Luke Milius <lmilius12@gmail.com>
2025-02-16 09:21:52 -0600

    Add systemd timer for img2mqtt service

--- flakes/hosts/util/default.nix
+++ flakes/hosts/util/default.nix
@@ -298,6 +298,31 @@
     ip rule add to 10.10.200.0/24 priority 2500 lookup main
   '';
 
+  # To check calendar times: 
+  # systemd-analyze calendar "*-*-* *:00:00" --iterations=10
+  systemd = {
+    timers."img2mqtt-10min" = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "*:0/10";
+        Persistent = true;
+      #   # OnBootSec = "1h";
+      #   # OnUnitActiveSec = "1h";
+      #   Unit = "img2mqtt-hourly";
+      };
+    };
+    services."img2mqtt-10min" = {
+        script = ''
+          /home/lmilius/workspace/py2mqtt/image_to_mqtt.sh cam_saylorville cam_i35 spc_activity mesonet_outlook mesonet_radar iowa_precip winter_roads winter_snowfall winter_midwest_snowfall winter_windchill winter_snowdepth wssi expected_snow expected_ice
+        '';
+        serviceConfig = {
+          Type = "oneshot";
+          User = "lmilius";
+        }
+    };
+  };
+
+
   # programs.bash.shellAliases = {
   #   l = "ls -alh";
   #   ll = "ls -l";
