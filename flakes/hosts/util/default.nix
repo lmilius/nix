0f9fefbe086fba71ca6a57d32f6206d2cdb7c723
Luke Milius <lmilius12@gmail.com>
2025-02-18 14:46:16 -0600

    try dot slash

--- flakes/hosts/util/default.nix
+++ flakes/hosts/util/default.nix
@@ -323,7 +323,7 @@
         # script = ''
         #   /home/lmilius/workspace/py2mqtt/image_to_mqtt.sh cam_saylorville cam_i35 spc_activity mesonet_outlook mesonet_radar iowa_precip winter_roads winter_snowfall winter_midwest_snowfall winter_windchill winter_snowdepth wssi expected_snow expected_ice
         # '';
-        ExecStart = "${import py2mqtt.nix {}}/bin/python image_to_mqtt.sh cam_saylorville";
+        ExecStart = "${import ./py2mqtt.nix {}}/bin/python image_to_mqtt.sh cam_saylorville";
         serviceConfig = {
           Type = "oneshot";
           User = "lmilius";
