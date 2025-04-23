f1d85df223b94e424fa2765e4d85597ec2afbdff
Luke Milius <lmilius12@gmail.com>
2025-04-23 12:50:23 -0500

    add formatter

--- flakes/flake.nix
+++ flakes/flake.nix
@@ -88,7 +88,7 @@
     packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
     # Formatter for your nix files, available through 'nix fmt'
     # Other options beside 'alejandra' include 'nixpkgs-fmt'
-    # formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
     # Your custom packages and modifications, exported as overlays
     overlays = import ./overlays {inherit inputs;};
