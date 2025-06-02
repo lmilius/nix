e8caff934d590719d5e01e5cdeb02a854135daa3
Luke Milius <lmilius12@gmail.com>
2025-06-02 08:50:45 -0500

    Add default branch to git config

--- flakes/users/lmilius/home.nix
+++ flakes/users/lmilius/home.nix
@@ -107,6 +107,9 @@ in
     userEmail = "lmilius12@gmail.com";
     userName = "Luke Milius";
     diff-so-fancy.enable = true;
+    extraConfig = ''
+      init.defaultBranch = "main"
+    '';
   };
   programs.home-manager.enable = true;
   programs.ssh = {
