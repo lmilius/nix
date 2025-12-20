38c1fdd97119b6caaaee5ec7424adb2ea8544025
Luke Milius <lmilius12@gmail.com>
2025-12-19 22:38:57 -0600

    Update flake pins to 25.11

--- flakes/flake.nix
+++ flakes/flake.nix
@@ -2,10 +2,10 @@
   description = "Nix Config";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.05";
+    nixpkgs.url = "nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
