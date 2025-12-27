28d99b768a5af2b24172669c75965febdfc060d5
Luke Milius <lmilius12@gmail.com>
2025-12-26 22:45:56 -0600

    remove gro-forwarding

--- flakes/hosts/parent-util/default.nix
+++ flakes/hosts/parent-util/default.nix
@@ -323,16 +323,17 @@
     interfaceName = "br0";
   };
 
-  services.networkd-dispatcher = {
-    enable = true;
-    rules."50-tailscale" = {
-      onState = [ "routable" ];
-      script = ''
-        NETDEV="$(ip -o route get 8.8.8.8 | cut -f 5 -d " ")"
-        "${pkgs.ethtool}/sbin/ethtool" -K "$NETDEV" rx-udp-gro-forwarding on rx-gro-list off
-      '';
-    };
-  };
+  # services.networkd-dispatcher = {
+  #   enable = true;
+  #   rules."50-tailscale" = {
+  #     onState = [ "routable" ];
+  #     script = ''
+  #       NETDEV="$(ip -o route get 8.8.8.8 | cut -f 5 -d " ")"
+  #       "${pkgs.ethtool}/sbin/ethtool" -K "$NETDEV" rx-udp-gro-forwarding on rx-gro-list off
+  #     '';
+  #   };
+  # };
+  
   # networking.firewall.checkReversePath = "loose";
 
   # Allows vscode remote ssh server to work when this machine is the server
