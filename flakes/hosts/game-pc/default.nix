5d80d6f72d3b3f848e968a324843cc8e64525a7e
Luke Milius <lmilius12@gmail.com>
2026-02-10 20:44:01 -0600

    Remove openwebui

--- flakes/hosts/game-pc/default.nix
+++ flakes/hosts/game-pc/default.nix
@@ -372,11 +372,11 @@
     host = "0.0.0.0";
     openFirewall = true;
   };
-  services.open-webui = {
-    enable = true;
-    host = "0.0.0.0";
-    openFirewall = true;
-  };
+  # services.open-webui = {
+  #   enable = true;
+  #   host = "0.0.0.0";
+  #   openFirewall = true;
+  # };
 
   # nix cli helper
   # https://github.com/viperML/nh
