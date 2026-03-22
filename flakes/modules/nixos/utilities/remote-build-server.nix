4f29ca42fa1632155517a81caaec6c5899c184c9
Luke Milius <lmilius12@gmail.com>
2026-03-22 13:59:11 -0500

    add system ssh keys to nix builder

--- flakes/modules/nixos/utilities/remote-build-server.nix
+++ flakes/modules/nixos/utilities/remote-build-server.nix
@@ -32,6 +32,10 @@ in
       extraGroups = [ "nixbld" ];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDAtjRZRmD5R38oShBAtJ0XjXdJWtz38Z6Vj6F1l0pYF lmilius@x1carbon"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDBltnMoyEQyhGuBJLw3UkhTfGmmuiHCV00/ErMPK3Y6 nas"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqqO9aLsBuZBE2Q1UkjpBQlAqWOuB5iGTOD2TewW0Lq parent-util"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBYW1f/8TDDBbFd5AnPYCok5sYGJCRNkMyeR38Gs4Q1+ util"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBmyQ2xYPwVp2gJmen/xrvBZo9nNH+2ryY/FVRLmS8r6 t480s"
       ];
     };
 
