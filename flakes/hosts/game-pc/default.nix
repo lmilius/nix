5ee3f27d72e874c9e1ba04722be3dd55c2134c56
Luke Milius <lmilius12@gmail.com>
2026-08-28 22:51:02 -0500

    syntax

--- flakes/hosts/game-pc/default.nix
+++ flakes/hosts/game-pc/default.nix
@@ -61,7 +61,7 @@
       # FORCE the 26.05 legacy driver profile for older cards
       # This prevents NixOS from grabbing the incompatible 580+ stable series
       # package = config.boot.kernelPackages.nvidiaPackages.legacy_535;
-      branch = "legacy_580"
+      branch = "legacy_580";
       modesetting.enable = true;
       powerManagement.enable = false;
       nvidiaSettings = true;
